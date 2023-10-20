068b601eb48097bc1e582cf0284eebb53674ace0
ducchienkma <chiendtak52@gmail.com>
2023-10-20 21:21:25 +0700

    f-commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = '20/10';
+const giftUrl = '';
+const eventName = `Happy women's day`;
+const titleCard = 'Gửi đến Mai';
+const contentCard = 'Chúc Mai ngày 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Mai trong hôm nay và cả những ngày sau...';
+const giftImage = '';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
